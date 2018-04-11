60b207a0642d44ced95a54f257dff86d30ebd105
gururaj <gururaj.bm@costrategix.com>
2018-04-11 19:36:22 +0530

    Test with add one element get that element

--- src/main/java/LinkedList/LinkedList.java
+++ src/main/java/LinkedList/LinkedList.java
@@ -1,14 +1,27 @@
 package LinkedList;
 
-public class LinkedList {
+public class LinkedList<T> {
 
     private int size = 0;
+    private Node first;
 
     public int size() {
         return size;
     }
 
-    public void add(String a) {
+    public void add(T value) {
         size++;
+        first = new Node(value);
+    }
+
+    public T get(int index) {
+        return first.value;
+    }
+
+    private class Node {
+        private final T value;
+        public Node(T value) {
+            this.value = value;
+        }
     }
 }
--- src/test/java/LinkedListTest.java
+++ src/test/java/LinkedListTest.java
@@ -1,19 +1,31 @@
 import LinkedList.LinkedList;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class LinkedListTest {
 
+    private LinkedList list;
+
+    @Before
+    public void setUp() {
+        list = new LinkedList<String>();
+    }
+
     @Test
-    public void testGet_initialListIsEmpty() {
-        LinkedList list = new LinkedList();
+    public void testSize_initialListIsEmpty() {
         Assert.assertEquals(0, list.size());
     }
 
     @Test
-    public void testGet_addFirstElementSizeIsOne() {
-        LinkedList list = new LinkedList();
+    public void testSize_oneElement_sizeIsOne() {
         list.add("a");
         Assert.assertEquals(1, list.size());
+    }
+
+    @Test
+    public void testGet_oneElement() {
+        list.add("a");
+        Assert.assertEquals("a", list.get(0));
     }
 }
